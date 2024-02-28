84c5bee231f85adb175da80115603bb88754c1a5
ruuuu <rufinka_91@mail.ru>
2024-02-28 15:40:49 +0300

    Add files via upload

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -3,22 +3,37 @@ const bgElements = () => {
    const elements = document.querySelectorAll('.set-bg');
 
 
-   // for(let i = 0; i < elements.length; i++){
-   //    console.dir(elements[i]);
-   
-   //    const src = elements[i].dataset.setbg;  // получаем значение дата атрибута data-setbg
-      
-   //    elements[i].style.backgroundImage = `url(${src})`;
-      
-   // }
-
+  
    elements.forEach((item) => {
 
       const src = item.dataset.setbg; 
       item.style.backgroundImage = `url(${src})`;
-   })
-   
+   });
+
+
+  
 }
 
 
 bgElements();
+
+
+
+// const array = [
+//    {
+//       id: 0,
+//       value: 100
+//    },
+//    {
+//       id: 1,
+//       value: 300
+//    },
+//    {
+//       id: 2,
+//       value: 200
+//    },
+// ];
+
+// const newArray = array.sort((a, b) => { // сортирует 
+//    return a.value - b.value;
+// });
--- js/mainData.js
+++ js/mainData.js
@@ -1,15 +1,61 @@
 const mainData = () => {
 
-  // всатвить урл из firebase
-  //     ./db.json
-   fetch('https://animejs-5b7c7-default-rtdb.firebaseio.com/db.json')
-      .then((response) => { // когда данные с сервера вернуться, запуститься then()-асинхронный
-         //console.log(response)
+   const renderAnimeList = (array, ganres) => {  // 6 жанров
 
+
+   }
+
+
+
+
+   const renderTopAnime = (array) => {
+
+      const wrapper = document.querySelector('.filter__gallery');
+      wrapper.innerHTML = '';    // очищаем контейнер
+
+      array.forEach((item) => {
+       
+         wrapper.insertAdjacentHTML('afterbegin', `
+               <div class="product__sidebar__view__item set-bg mix" data-setbg="${item.image}">
+                  <div class="ep"> ${item.rating} / 10 </div>
+                  <div class="view"><i class="fa fa-eye"></i> ${item.views} </div>
+                  <h5><a href="/anime-details.html"> ${item.title} </a></h5>
+               </div>
+            `  
+         );
+      });
+
+
+      
+
+      wrapper.querySelectorAll('.set-bg').forEach((elem) => {
+   
+         elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
+      });
+
+   }
+
+ 
+
+  //     ./db.json
+   fetch('https://animejs-5b7c7-default-rtdb.firebaseio.com/db.json')    // всатвить урл из firebase
+      .then((response) => { // когда данные с сервера вернуться, запуститься then()-асинхронный
          return response.json();
       })
       .then((data) => {   // data - response.json()
-         console.log(data)
+
+         const ganres = new Set();  // коллекция, хранит уникальные значения
+
+
+
+
+         const sortArray = data.sort((a, b) => { // сортировка по убыванию
+            return b.views - a.views;
+         });
+
+         renderTopAnime(sortArray.slice(0, 5));  // slice(startIndex, endIndex) вырезает часть массива
+
+         
       })
 
 
