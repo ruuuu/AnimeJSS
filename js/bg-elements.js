04ddcdb4221ec0fdd72200a1699e0b4d3a09164b
ruuuu <rufinka_91@mail.ru>
2024-02-28 14:34:17 +0300

    Add files via upload

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -3,15 +3,20 @@ const bgElements = () => {
    const elements = document.querySelectorAll('.set-bg');
 
 
-   for(let i = 0; i < elements.length; i++){
-      //console.dir(elements[i]);
+   // for(let i = 0; i < elements.length; i++){
+   //    console.dir(elements[i]);
    
-      const src = elements[i].dataset.setbg;  // получаем значение дата атрибута data-setbg
+   //    const src = elements[i].dataset.setbg;  // получаем значение дата атрибута data-setbg
       
-      //elements[i].style.backgroundImage = 'url(' + src + ')';
-      elements[i].style.backgroundImage = `url(${src})`;
-      //console.dir(elements[i])
-   }
+   //    elements[i].style.backgroundImage = `url(${src})`;
+      
+   // }
+
+   elements.forEach((item) => {
+
+      const src = item.dataset.setbg; 
+      item.style.backgroundImage = `url(${src})`;
+   })
    
 }
 
--- js/mainData.js
+++ js/mainData.js
@@ -2,14 +2,14 @@ const mainData = () => {
 
   // всатвить урл из firebase
   //     ./db.json
-   fetch('https://anime-d6a5d-default-rtdb.firebaseio.com/')
+   fetch('https://animejs-5b7c7-default-rtdb.firebaseio.com/db.json')
       .then((response) => { // когда данные с сервера вернуться, запуститься then()-асинхронный
          //console.log(response)
 
          return response.json();
       })
       .then((data) => {   // data - response.json()
-         console.log(data.anime)
+         console.log(data)
       })
 
 
